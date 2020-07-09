23cc1692e4b12a90cf5cdd409163fe29a3be8de4
Ganesh Hegde <hegde.ganesh97@gmail.com>
2020-07-09 20:36:35 +0530

    SQL dump and changes in readme.md (Pre-final)

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,6 +1,9 @@
 <footer class="navbar navbar-default navbar-fixed-bottom">
     <div class="container-fluid">
-        <a href="https://github.com/ganesh-hegde97/lsapp.git" class="navbar-text pull-right"> &#127760; Open source project on github - LaraBlog </a>
         <p class="navbar-text pull-left"><strong>Made with &#128420; by Ganesh Hegde</strong></p>
+        <a href="https://github.com/ganesh-hegde97/lsapp.git" class="navbar-text pull-right">
+            &#127760; Open source project on github - LaraBlog
+            <span class="text-danger">Version 1.0.0</span>
+        </a>
     </div>
 </footer>
